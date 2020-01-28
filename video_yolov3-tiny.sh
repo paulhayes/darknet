26b454ecde20b3a93ceb182c6c97979c71201687
paulhayes <paul.hayes@entropedia.co.uk>
2020-01-28 15:09:33 +0000

    Added test script for yolov3 tiny

---
+++ video_yolov3-tiny.sh
@@ -0,0 +1,6 @@
+
+
+./darknet detector demo ./cfg/coco.data ./cfg/yolov3-tiny.cfg ./yolov3-tiny.weights /media/sandisk/2020-01-11/15fps/out1-20-01-11_12-31-06.avi -i 0 -thresh 0.25 -mjpeg_port 8090 -dont_show
+
+
+
